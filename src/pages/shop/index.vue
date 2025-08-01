9d49ec5096089e21edbaec6e609b7d728904e157
lf <lfnothing00@gmail.com>
2025-08-01 15:23:17 +0800

    feat(shop): 添加了用户地址定位功能

--- src/pages/shop/index.vue
+++ src/pages/shop/index.vue
@@ -3,31 +3,70 @@
 /* eslint no-console: ["error", { allow: ["warn", "error", "log"] }] */
 
 import { ref } from 'vue'
-import { ElAmap } from '@vuemap/vue-amap'
+import { ElAmap, lazyAMapApiLoaderInstance, useGeolocation } from '@vuemap/vue-amap'
 import type { ElAmapInstance } from '@vuemap/vue-amap'
 
-const zoom = ref(12)
-const center = ref([121.59996, 31.197646])
+interface StateItem {
+  zoom: number
+  center: [number, number]
+  position: [number, number]
+  markerVisible: true
+  markDraggable: false
+}
+
+const baseState = ref<StateItem>({
+  zoom: 12,
+  center: [116.397428, 39.90923],
+  position: [116.397428, 39.90923],
+  markerVisible: true,
+  markDraggable: false,
+})
+
+// 这样就可以获取到ElAmap的组件实例类型
+const mapRef = ref<ElAmapInstance>(null)
+
 function init(map: AMap.Map) {
   console.log('map init: ', map)
 }
 
-// 这样就可以获取到ElAmap的组件实例类型
-const mapRef = ref<ElAmapInstance>(null)
+onBeforeMount(() => {
+  lazyAMapApiLoaderInstance.then(() => {
+    useGeolocation({
+      enableHighAccuracy: true,
+      needAddress: true,
+    }).then((res) => {
+      const { getCurrentPosition, getCityInfo } = res
+      getCurrentPosition().then((currentPosition) => {
+        baseState.value.center = currentPosition.position.toArray()
+        baseState.value.position = currentPosition.position.toArray()
+        console.log('currentPosition: ', currentPosition)
+      })
+      getCityInfo().then((cityResult) => {
+        console.log('cityResult: ', cityResult)
+      })
+    })
+  })
+})
 </script>
 
 <template>
   <div class="map-page-container">
     <ElAmap
       ref="mapRef"
-      :center="center"
-      :zoom="zoom"
+      :center="baseState.center"
+      :zoom="baseState.zoom"
       @init="init"
-    />
+    >
+      <ElAmapMarker
+        :position="baseState.center"
+        :visible="baseState.markerVisible"
+        :draggable="baseState.markDraggable"
+      />
+    </ElAmap>
   </div>
 </template>
 
-<style>
+<style lang="less">
 .map-page-container {
   width: 100%;
   height: 100vw;
